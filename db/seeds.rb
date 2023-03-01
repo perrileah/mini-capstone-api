84d56e5821fae6deb0f20cde64fbf5c4ef267dd5
Leah Perri <perrileah2023@gmail.com>
2023-03-01 12:40:04 -0600

    products created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+products = Product.create([{ name: "Banana", price: 2.00, image_url: "banana_image_url", description: "a yellow, edible fruit that is botanically a berry" }, { name: "Apple", price: 1.00, image_url: "apple_image_url", description: "an edible fruit of the rose family" }, { name: "Frozen_Pizza", price: 4.00, image_url: "frozen_pizza_url", description: "a simple and delicious dinner" }, { name: "Eggs", price: 5.00, image_url: "eggs_image_url", description: "oval object that cracks open" }])
