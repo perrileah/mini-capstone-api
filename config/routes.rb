a29d0ba779bc648d8c740071dbba17d8c211f8a8
Leah Perri <perrileah2023@gmail.com>
2023-03-09 13:09:20 -0600

    add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
-
   get "/products/:id" => "products#show"
-
   post "/products" => "products#create"
-
   patch "/products/:id" => "products#update"
-
   delete "products/:id" => "products#destroy"
+
+  post "/users" => "users#create"
 end
