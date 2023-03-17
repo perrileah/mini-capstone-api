e0d50b9156bb63ec33001144eb451a94425f2471
Leah Perri <perrileah2023@gmail.com>
2023-03-17 13:05:27 -0500

    current user carted products where status is carted!

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -8,22 +8,22 @@ class CartedProductsController < ApplicationController
     )
     if @carted_product.save
       render :show
-    else #sad pathmodf
+    else #sad path
       render json: { errors: @carted_product.errors.full_messages }
     end
   end
 
   def index
     if current_user
-      @carted_products = current_user.carted_products
+      @carted_products = current_user.carted_products.where(status: "carted")
       render :index
     else
       render json: [], status: :unauthorized
     end
   end
-
-  # def show
-  #   @carted_product = CartedProduct.find_by(id: params[:id])
-  #   render :show
-  # end
 end
+
+# def show
+#   @carted_product = CartedProduct.find_by(id: params[:id])
+#   render :show
+# end
